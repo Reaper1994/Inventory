895bb977ecf5975c77c972e57868cad0f1cdd882
Osmar Rodrigues <osmarrod18@gmail.com>
2018-10-16 03:01:49 +0530

    Update addmodel.php
    added option to upload file

--- addmodel.php
+++ addmodel.php
@@ -13,26 +13,58 @@ require_once(dirname(dirname(__FILE__)).'\Carinventory\BrandClass.php');
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+  <style type="text/css"> .btn-file {
+    position: relative;
+    overflow: hidden;
+}
+.btn-file input[type=file] {
+    position: absolute;
+    top: 0;
+    right: 0;
+    min-width: 100%;
+    min-height: 100%;
+    font-size: 100px;
+    text-align: right;
+    filter: alpha(opacity=0);
+    opacity: 0;
+    outline: none;
+    background: white;
+    cursor: inherit;
+    display: block;
+}
+
+#img-upload{
+    width: 100%;
+}</style>
+
   <script>
       $(document).ready(function () {
 
- 
 
-        $('form#add_model').on('submit', function (e) {
+  $('form#add_model').on('submit',(function(e) {
+        e.preventDefault();
+        var formData = new FormData(this);
 
-          e.preventDefault();
-
-          $.ajax({
-            type: 'post',
+        $.ajax({
+            type:'POST',
             url: 'save.php',
-            data: $('form#add_model').serialize(),
-            success: function (response) {
-              console.log('add_manufacturer request sent');
-              $("#result").html(response);
+            data:formData,
+            cache:false,
+            contentType: false,
+            processData: false,
+            success:function(data){
+                console.log("success");
+                console.log(data);
+                $("#result").html(data);
+            },
+            error: function(data){
+                console.log("error");
+                console.log(data);
             }
-          })
-
         });
+    }));
+
+
 
       });
     </script>
@@ -42,7 +74,7 @@ require_once(dirname(dirname(__FILE__)).'\Carinventory\BrandClass.php');
 
 <div class="container">
   <h2>Add Models</h2>
-  <form id="add_model" >
+  <form id="add_model" enctype="multipart/form-data" >
 
   	<input type="hidden"  name="type" value="modelfrom">
 
@@ -58,14 +90,17 @@ require_once(dirname(dirname(__FILE__)).'\Carinventory\BrandClass.php');
 
 <?php
 
-$query = "SELECT id,manufacturer FROM car_master ORDER BY id ASC";
-$result = $crud->getData($query);
- $html="<option ></option>";
-foreach ($result as $key => $res) {
-  
-    $html.="<option value=".$res['id'].">".$res['manufacturer']."</option>";
-  }
-echo $html;
+   $result=$crud->Check_existing_data('car_master','id,manufacturer',array('del'=> 0));
+
+
+
+
+
+
+
+echo $car->build_manufacturer_dropdown($result);
+
+
 ?>
       
                 </select>
@@ -77,6 +112,20 @@ echo $html;
     	<input type="text" class="form-control" id="model" placeholder="Model Name" name="model">
     </div>
       </div>
+
+
+  <!-- COMPONENT START -->
+    <div class="form-group">
+        <label>Upload Image</label>
+     <!--    <div class="input-group"> -->
+          
+               <input type="file" class="form-control" name="fileToUpload" id="fileToUpload">
+    <!-- <input type="submit" value="Upload Image" name="submit"> -->
+            
+            <!-- <input type="text" name="attach" value="" class="form-control" readonly> -->
+      <!--   </div> -->
+        <img id='img-upload'/>
+    </div>
 
         <div id="result">
     
