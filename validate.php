7727a57f8e5b90c4bbde0ef7f58711caaa4d16c2
Osmar Rodrigues <osmarrod18@gmail.com>
2018-10-16 03:08:31 +0530

    Update validate.php
    version 2,0

--- validate.php
+++ validate.php
@@ -2,15 +2,22 @@
 
 class Validate 
 {
+
+
+
+ public $iteration=0;
+
     public function check_empty($data, $fields)
     {
         $msg = "";
+        $this->iteration++;
         foreach ($fields as $value) {
             if (empty($data[$value])) {
 
             	/*print_r($data);*/
-                $msg .= "<div class='alert alert-warning'>Field Empty</div>";
+                $msg .= "<div class='alert alert-warning'>Field  $this->iteration Empty</div>";
             }
+            $this->iteration++;   
         } 
         return $msg;
     }
