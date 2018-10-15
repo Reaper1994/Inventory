cb4802f503f7440f4e7f41c280744996a2951695
Osmar Rodrigues <osmarrod18@gmail.com>
2018-10-16 03:09:44 +0530

    version 2.0

--- lister.php
+++ lister.php
@@ -2,7 +2,7 @@
 
 <?php 
 
-    require_once(dirname(dirname(__FILE__)).'\Carinventory\configuration.php');
+require_once(dirname(dirname(__FILE__)).'\Carinventory\configuration.php');
 require_once(dirname(dirname(__FILE__)).'\Carinventory\BrandClass.php');
 ini_set('display_errors', 1);
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
@@ -43,38 +43,16 @@ $update="update  car_stock set qty='$qty' where id=".$id;
 
 
 
-$query = "SELECT car_stock.id, manufacturer,model,qty FROM car_master inner join car_stock on car_stock.mf_id=car_master.id  where car_master.del=0  and qty>0 and car_stock.del=0 ";
+ $query = "SELECT car_stock.id, manufacturer,model,qty FROM car_master inner join car_stock on car_stock.mf_id=car_master.id  where car_master.del=0  and qty>0 and car_stock.del=0 ";
 
 
 $result = $crud->getData($query);
 
-$ctr=1;
-$html="<table class='table'>
-    
-      <tr>
-        <th>Number</th>
-        <th>Manufacturer Name</th>
-        <th>Model Name</th>
-        <th>Count</th>
-        <th>Sold Button</th>
-      </tr> <tbody>";
-
-foreach ($result as $key => $res) {
 
 
-  $html.=" 
-   
-    <tr><td>".$ctr++."</td><td>".$res['manufacturer']."</td><td>".$res['model']."</td><td>".$res['qty']."</td> <td><button type='button' id='button".$res['id']."'  name='button".$res['id']."' onclick='delete(this.value)' value=".$res['id']." class='btn btn-success btn_remove'>Sold</button></td></tr></tr>";
+echo $car->getlist($result);
 
-  }
-
-  $html.="</span>
-    </tbody>
-  </table>";
-  
-
-  echo $html;
-
+ 
    
 
           
